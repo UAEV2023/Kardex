af1de2b6f50e8a21777155e9149a72c23601ebb4
César González <chesarglez429@gmail.com>
2023-06-21 13:47:10 -0600

    Updates

---
+++ precache-manifest.0afa5f0111827c11c1a939ef87e0aecf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/Kardex/favicon.ico"
+  },
+  {
+    "revision": "9fc9d5840c4bdc686d66def295ceac51",
+    "url": "/Kardex/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/Kardex/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/Kardex/manifest.json"
+  },
+  {
+    "revision": "c29b473d2932b8b37a47",
+    "url": "/Kardex/static/css/main.8841eba0.chunk.css"
+  },
+  {
+    "revision": "c29b473d2932b8b37a47",
+    "url": "/Kardex/static/js/main.c29b473d.chunk.js"
+  },
+  {
+    "revision": "5e3f2703957ab70aa55e",
+    "url": "/Kardex/static/js/runtime~main.5e3f2703.js"
+  },
+  {
+    "revision": "b17b319b1ad2b352f0ab",
+    "url": "/Kardex/static/js/vendors~main.b17b319b.chunk.js"
+  }
+]);
